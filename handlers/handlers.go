f38bdde7402d636c2d7339ecac148fd717752f92
aaron.lee <aaron.lee@shopee.com>
2022-08-16 18:10:17 +0700

    add handlers for auth and wallet

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,4 +1,20 @@
 package handlers
 
+import "git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/services"
+
 type Handler struct {
+	authService   services.AuthService
+	walletService services.WalletService
+}
+
+type HandlerConfig struct {
+	AuthService   services.AuthService
+	WalletService services.WalletService
+}
+
+func New(c *HandlerConfig) *Handler {
+	return &Handler{
+		authService:   c.AuthService,
+		walletService: c.WalletService,
+	}
 }
