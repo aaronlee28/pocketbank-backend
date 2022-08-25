ade8787f873f9e8f5c445c2b59de8e8f6e2a5a24
aaron.lee <aaron.lee@shopee.com>
2022-08-25 19:38:14 +0700

    feat: add interest rate to deposit

--- dto/get_depositinfo_res.go
+++ dto/get_depositinfo_res.go
@@ -7,11 +7,15 @@ import (
 type DepositInfoRes struct {
 	Balance       float32 `json:"balance"`
 	DepositNumber int     `json:"depositNumber"`
+	InterestRate  float32 `json:"interestRate"`
+	Interest      float32 `json:"interest"`
 }
 
 func (_ *DepositInfoRes) FromDepositInfo(t *models.Deposit) *DepositInfoRes {
 	return &DepositInfoRes{
 		Balance:       t.Balance,
 		DepositNumber: t.DepositNumber,
+		InterestRate:  t.InterestRate,
+		Interest:      t.Interest,
 	}
 }
