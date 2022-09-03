d76fd15e86e3fa84fe54a524dd1e5550481de18a
aaron.lee <aaron.lee@shopee.com>
2022-09-03 18:54:29 +0700

    feat: add positive and negative test for wallet service - user detials, transactionhistory

--- services/auth_service_test.go
+++ services/auth_service_test.go
@@ -139,3 +139,163 @@ func TestAuthService_Register(t *testing.T) {
 		assert.Nil(t, res)
 	})
 }
+
+func TestAuthService_SignIn(t *testing.T) {
+	t.Run("should return response body when given email and password", func(t *testing.T) {
+		email := "test@gmail.com"
+
+		request := dto.AuthReq{
+			Email:    email,
+			Password: "test",
+		}
+		ea := &email
+		user := models.User{
+			Email:    ea,
+			Password: "test",
+		}
+
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("MatchingCredential", "test@gmail.com", "test").Return(&user, nil, true)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		idToken, err := authService.SignIn(&request)
+		assert.Nil(t, err)
+		assert.NotNil(t, idToken)
+	})
+	t.Run("should return error when isUserActive == false", func(t *testing.T) {
+		email := "test@gmail.com"
+
+		request := dto.AuthReq{
+			Email:    email,
+			Password: "test",
+		}
+
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("MatchingCredential", "test@gmail.com", "test").Return(nil, nil, false)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		idToken, err := authService.SignIn(&request)
+		assert.Nil(t, idToken)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("should return error when error is not nil", func(t *testing.T) {
+		email := "test@gmail.com"
+
+		request := dto.AuthReq{
+			Email:    email,
+			Password: "test",
+		}
+		response := httperror.AppError{
+			Message: "test error",
+		}
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("MatchingCredential", "test@gmail.com", "test").Return(nil, response, true)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		idToken, err := authService.SignIn(&request)
+		assert.Nil(t, idToken)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestAuthService_GetCode(t *testing.T) {
+	t.Run("should return response body when given email and password", func(t *testing.T) {
+		email := "test"
+		getCodeRequest := dto.CodeReq{
+			Email: email,
+		}
+		ea := &email
+		user := models.User{
+			Email: ea,
+		}
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("GetCode", "test").Return(&user, 0, nil)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		res, err := authService.GetCode(&getCodeRequest)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+	})
+
+	t.Run("should return error when email is incorrect", func(t *testing.T) {
+		email := ""
+		getCodeRequest := dto.CodeReq{
+			Email: email,
+		}
+		var user *models.User
+		user = nil
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("GetCode", "").Return(user, 1, nil)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		res, err := authService.GetCode(&getCodeRequest)
+		assert.NotNil(t, err)
+		assert.Nil(t, res)
+	})
+}
+
+func TestAuthService_ChangePassword(t *testing.T) {
+	t.Run("should return response body when given email and password", func(t *testing.T) {
+		changePassReq := dto.ChangePReq{
+			Email:       "email@gmail.cxom",
+			NewPassword: "password",
+			Code:        12345,
+		}
+		expectedRet := dto.ChangePRes{Success: "Successfully Change Password"}
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("ChangePassword", &changePassReq).Return(0)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		ret, err := authService.ChangePassword(&changePassReq)
+		assert.Nil(t, err)
+		assert.NotNil(t, ret)
+		assert.Equal(t, &expectedRet, ret)
+	})
+
+	t.Run("should return error when repo returns 1", func(t *testing.T) {
+		changePassReq := dto.ChangePReq{
+			Email:       "email@gmail.cxom",
+			NewPassword: "password",
+			Code:        12345,
+		}
+
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("ChangePassword", &changePassReq).Return(1)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		_, err := authService.ChangePassword(&changePassReq)
+		assert.ErrorContains(t, err, "Email is not found")
+	})
+
+	t.Run("should return error when repo returns 2", func(t *testing.T) {
+		changePassReq := dto.ChangePReq{
+			Email:       "email@gmail.cxom",
+			NewPassword: "password",
+			Code:        12345,
+		}
+
+		mockRepo := new(mocks.AuthRepository)
+		mockRepo.On("ChangePassword", &changePassReq).Return(2)
+		authService := services.NewAuthService(&services.ASConfig{
+			AuthRepository: mockRepo,
+		})
+
+		_, err := authService.ChangePassword(&changePassReq)
+		assert.ErrorContains(t, err, "Code Invalid")
+	})
+}
--- services/wallet_service_test.go
+++ services/wallet_service_test.go
@@ -1,1 +1,120 @@
 package services_test
+
+import (
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/dto"
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/httperror"
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/mocks"
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/models"
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/repositories"
+	"git.garena.com/sea-labs-id/batch-01/aaron-lee/final-project-backend/services"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestWalletService_TransactionHistory(t *testing.T) {
+	t.Run("Should return response body", func(t *testing.T) {
+		request := &repositories.Query{
+			SortBy:     "created_at",
+			Sort:       "desc",
+			Limit:      "",
+			Page:       "",
+			Search:     "",
+			FilterTime: "74000",
+			MinAmount:  "0",
+			MaxAmount:  "999999999999",
+			Type:       "",
+		}
+		var repoResponse []models.Transaction
+		resp := models.Transaction{
+			Id:                   0,
+			SenderWalletNumber:   0,
+			ReceiverWalletNumber: 0,
+			SenderName:           "",
+			ReceiverName:         "",
+			Amount:               0,
+			Description:          "",
+			CreatedAt:            time.Time{},
+			Type:                 "",
+			Status:               "",
+		}
+		tr := dto.TransRes{
+			SenderWalletNumber:   0,
+			ReceiverWalletNumber: 0,
+			SenderName:           "",
+			ReceiverName:         "",
+			Amount:               0,
+			Type:                 "",
+			Status:               "",
+			Description:          "",
+			CreatedAt:            time.Time{},
+		}
+		var tra []dto.TransRes
+		tra = append(tra, tr)
+		response := dto.TransHistoryRes{
+			TotalLength:  1,
+			Transactions: tra,
+		}
+		repoResponse = append(repoResponse, resp)
+		mockRepo := new(mocks.WalletRepository)
+		mockRepo.On("TransactionHistory", request, 0).Return(1, &repoResponse, nil)
+		walletService := services.NewWalletServices(&services.WSConfig{
+			WalletRepository: mockRepo,
+		})
+		res, err := walletService.TransactionHistory(request, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, res, &response)
+
+	})
+
+	t.Run("Should return error when error != nil", func(t *testing.T) {
+		request := &repositories.Query{
+			SortBy:     "created_at",
+			Sort:       "desc",
+			Limit:      "",
+			Page:       "",
+			Search:     "",
+			FilterTime: "74000",
+			MinAmount:  "0",
+			MaxAmount:  "999999999999",
+			Type:       "",
+		}
+		response := httperror.AppError{
+			Message: "users_contact_uindex",
+		}
+		mockRepo := new(mocks.WalletRepository)
+		mockRepo.On("TransactionHistory", request, 0).Return(0, nil, response)
+		walletService := services.NewWalletServices(&services.WSConfig{
+			WalletRepository: mockRepo,
+		})
+		_, err := walletService.TransactionHistory(request, 0)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestWalletService_UserDetails(t *testing.T) {
+	t.Run("Should return response body", func(t *testing.T) {
+		repoResponse := dto.UserDetailsRes{}
+		expectedServiceResponse := dto.UserDetailsRes{}
+		mockRepo := new(mocks.WalletRepository)
+		mockRepo.On("UserDetails", 0).Return(&repoResponse, nil)
+		walletService := services.NewWalletServices(&services.WSConfig{
+			WalletRepository: mockRepo,
+		})
+		res, err := walletService.UserDetails(0)
+		assert.Nil(t, err)
+		assert.Equal(t, res, &expectedServiceResponse)
+	})
+	t.Run("Should return error when repoo returns error", func(t *testing.T) {
+		response := httperror.AppError{
+			Message: "users_contact_uindex",
+		}
+		mockRepo := new(mocks.WalletRepository)
+		mockRepo.On("UserDetails", 0).Return(nil, response)
+		walletService := services.NewWalletServices(&services.WSConfig{
+			WalletRepository: mockRepo,
+		})
+		_, err := walletService.UserDetails(0)
+		assert.NotNil(t, err)
+	})
+}
