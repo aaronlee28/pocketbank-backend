e6eec4ec48fb76c82cb5937300973d61d99e9199
aaron.lee <aaron.lee@shopee.com>
2022-09-03 16:33:25 +0700

    feat: add positive and negative test for admin handlers - get merch stock

--- handlers/admin_handler_test.go
+++ handlers/admin_handler_test.go
@@ -720,3 +720,45 @@ func TestHandler_UpdateMerchStocks(t *testing.T) {
 		assert.Equal(t, responseError, rec.Body.String())
 	})
 }
+
+func TestHandler_GetMerchStock(t *testing.T) {
+	t.Run("should return successful when payload is given", func(t *testing.T) {
+		response := models.Merchstock{
+			Id:         1,
+			Name:       "test",
+			StockCount: 1,
+		}
+		var responseArray []models.Merchstock
+		responseArray = append(responseArray, response)
+		responseSuccess := httpsuccess.AppSuccess{
+			StatusCode: 200,
+			Message:    "Ok",
+			Data:       responseArray,
+		}
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("GetMerchStock").Return(&responseArray, nil)
+
+		res, _ := json.Marshal(&responseSuccess)
+		req, _ := http.NewRequest(http.MethodGet, "/getmerchstock", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, string(res), rec.Body.String())
+	})
+
+	t.Run("should return error when error is not nil", func(t *testing.T) {
+		response := httperror.AppError{
+			Message: "Test Error",
+		}
+		responseError := ("{\"error\":\"Test Error\"}")
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("GetMerchStock").Return(nil, response)
+		req, _ := http.NewRequest(http.MethodGet, "/getmerchstock", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, responseError, rec.Body.String())
+	})
+}
