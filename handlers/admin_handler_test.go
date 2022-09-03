9580e50f217f6eafecc591bc6c7a16cd01d6bf0b
aaron.lee <aaron.lee@shopee.com>
2022-09-03 15:42:58 +0700

    feat: add positive and negative test for admin handlers - admin user referral details

--- handlers/admin_handler_test.go
+++ handlers/admin_handler_test.go
@@ -127,3 +127,87 @@ func TestHandler_AdminUserTransaction(t *testing.T) {
 		assert.Equal(t, responseError, rec.Body.String())
 	})
 }
+
+func TestHandler_AdminUserDetails(t *testing.T) {
+	t.Run("should return successful when payload is given", func(t *testing.T) {
+		response := dto.UserDetailsRes{
+			Name:           "test",
+			Email:          "test",
+			Contact:        "test",
+			ProfilePicture: "test",
+			ReferralNumber: 0,
+			SavingsNumber:  0,
+		}
+
+		responseSuccess := httpsuccess.AppSuccess{
+			StatusCode: 200,
+			Message:    "Ok",
+			Data:       response,
+		}
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("AdminUserDetails", 0).Return(&response, nil)
+
+		res, _ := json.Marshal(&responseSuccess)
+		req, _ := http.NewRequest(http.MethodGet, "/userdetails/:id", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, string(res), rec.Body.String())
+	})
+
+	t.Run("should return error when error is not nil", func(t *testing.T) {
+		response := httperror.AppError{
+			Message: "Test Error",
+		}
+		responseError := ("{\"error\":\"Test Error\"}")
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("AdminUserDetails", 0).Return(nil, response)
+		req, _ := http.NewRequest(http.MethodGet, "/userdetails/:id", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, responseError, rec.Body.String())
+	})
+}
+
+func TestHandler_AdminUserReferralDetails(t *testing.T) {
+	t.Run("should return successful when payload is given", func(t *testing.T) {
+		response := dto.UserReferralDetailsRes{
+			Count:       0,
+			ListOfUsers: nil,
+		}
+
+		responseSuccess := httpsuccess.AppSuccess{
+			StatusCode: 200,
+			Message:    "Ok",
+			Data:       response,
+		}
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("AdminUserReferralDetails", 0).Return(&response, nil)
+
+		res, _ := json.Marshal(&responseSuccess)
+		req, _ := http.NewRequest(http.MethodGet, "/userreferraldetails/:id", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, string(res), rec.Body.String())
+	})
+
+	t.Run("should return error when error is not nil", func(t *testing.T) {
+		response := httperror.AppError{
+			Message: "Test Error",
+		}
+		responseError := ("{\"error\":\"Test Error\"}")
+		mockService := new(mocks.AdminService)
+		router := &server.RouterConfig{AdminService: mockService}
+		mockService.On("AdminUserReferralDetails", 0).Return(nil, response)
+		req, _ := http.NewRequest(http.MethodGet, "/userreferraldetails/:id", testutils.MakeRequestBody(nil))
+		_, rec := testutils.ServeReq(router, req)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, responseError, rec.Body.String())
+	})
+}
